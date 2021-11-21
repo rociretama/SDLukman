29401255a3c3bee90b5f883a6219b3212aaf9eab
rociretama <rentamaroci@gmail.com>
2021-11-21 20:59:12 +0700

    fitur tambah guru

--- app/Controllers/Guru.php
+++ app/Controllers/Guru.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Controllers;
+
 use App\Models\GuruModel;
 
 class Guru extends BaseController
@@ -8,34 +9,38 @@ class Guru extends BaseController
     protected $guruModel;
     public function __construct()
     {
-       $this->guruModel = new GuruModel();
+        $this->guruModel = new GuruModel();
     }
     public function index()
     {
 
-             
         $data = [
-            'judul'=>'Data Guru',
-            'guru'=> $this->guruModel->getGuru()        
+            'judul' => 'Data Guru | Yayasan Lukman Nur Al-Hakim',
+            'guru' => $this->guruModel->getGuru()
 
         ];
-      
-        
-    return view('guru/index',$data);
+
+        return view('guru', $data);
+    }
+    
+
+
+    public function detail($slug)
+    {
+
+        $data = [
+            'judul' => 'Detail Guru | Data Guru',
+            'guru' => $this->guruModel->getGuru($slug)
+
+        ];
+        return view('guru/detail', $data);
+    }
+    public function create()
+    {
+        $data = [
+            'judul' => 'Form Tambah Data | Data Guru'
+        ];
+        return view('guru/create',$data);
     }
 
-    public function detail($slug){
-      
-       $data =[
-           'judul' => 'Detail Guru',
-           'guru' => $this->guruModel->getGuru($slug)
-           
-
-       ];
-      
-       return view('guru/detail',$data);
-       
-
-    
-   }
 }
---
+++ app/Views/guru/create.php
@@ -0,0 +1,11 @@
+<?= $this->extend('layout/template') ?>
+<?= $this->section('isi') ?>
+
+<div class="container">
+    <div class="row">
+        <div class="col-8">
+        <h2 class="my-3"> Form Tambah Data</h2>
+        </div>
+    </div>
+</div>
+<?= $this->endSection(); ?>
